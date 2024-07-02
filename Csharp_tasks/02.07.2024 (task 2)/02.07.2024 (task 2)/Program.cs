63be79e20ac534684839ddc422e835bbb2549e2b
BassamBanyAli <bassambanyali@gmail.com>
2024-07-02 16:44:01 -0700

    6 task

---
+++ Csharp_tasks/02.07.2024 (task 2)/02.07.2024 (task 2)/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface ISound
+{
+    void MakeSound();
+}
+
+public abstract class Animal
+{
+
+    public Animal(string name)
+    {
+        Name = name;
+    }
+    public string Name { get; set; }
+
+   public abstract void Eat();
+    public abstract void Sleep();
+}
+public class Dog:Animal,ISound {
+    public Dog(string name) : base(name)
+    {
+        Name=name;
+    }
+    public override void Eat()
+    {
+        Console.WriteLine($"the animals {Name} eating tomatto ");
+    }
+    public override void Sleep()
+    {
+        Console.WriteLine($"the animals is {Name} is sleeping");
+    }
+     public void MakeSound()
+    {
+        Console.WriteLine($" the animal is {Name} the sound is Woof!");
+    }
+    
+
+    
+}
+
+public class Cat : Animal, ISound
+{
+    public Cat(string name) : base(name)
+    {
+        Name = name;
+    }
+    public override void Eat()
+    {
+        Console.WriteLine($"the animals {Name} eating tomatto ");
+    }
+    public override void Sleep()
+    {
+        Console.WriteLine($"the animals is {Name} is sleeping");
+    }
+    public void MakeSound()
+    {
+        Console.WriteLine($" the animal is {Name} the sound is Meow!");
+    }
+
+
+
+}
+
+
+
+
+namespace _02._07._2024__task_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("cat");
+            Dog dog = new Dog("dog");
+            cat.Eat();
+            cat.Sleep();
+            cat.MakeSound();
+            dog.Eat();
+            dog.Sleep();
+            dog.MakeSound();
+        }
+    }
+}
---
+++ Csharp_tasks/02.07.2024/02.07.2024/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+ class Car
+{
+    string make;
+    string year;
+    string type;
+    string price;
+    string model;
+    string pallet_no;
+    string color;
+    public Car( string make,string year, string type,string price,string model,string pallet_no,string color)
+    {
+        this.make = make;
+        this.year = year;
+        this.type = type;
+        this.price = price;
+        this.model = model;
+        this.pallet_no = pallet_no;
+        this.color = color;
+        
+    }
+    public Car() { }
+    public void DisplayInfo(string make,string year)
+    {
+        this.make = make;
+        this.year = year;
+        Console.WriteLine($"the make's car is {this.make} and the year is {this.year}");
+    }
+    public void DisplayInfo(string make)
+    {
+        this.make = make;
+        Console.WriteLine($"the make's car is {this.make} ");
+    }
+    public void Display()
+    {
+        Console.WriteLine("Welcome Car");
+    }
+
+
+
+}
+
+
+ class BMW:Car
+{
+     public BMW (string make, string year, string price, string model, string pallet_no, string color)
+        :base(make,year,"BMW",price,model,pallet_no,color)
+    {
+
+    }
+    public BMW() { }
+    public void Display()
+    {
+        Console.WriteLine("Welcome BMW");
+    }
+}
+
+
+namespace _02._07._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            BMW car2 = new BMW();
+            car1.Display();
+            car2.Display();
+        }
+    }
+}
